3adec67daa374f509e4ee0555bef9f77448c3ba5
michael <4apaev@gmail.com>
2016-05-09 02:09:53 +0300

    remove amd export

--- dot.js
+++ dot.js
@@ -1,42 +1,34 @@
-(function(root) {
-  'use strict';
-  let is = x => x!=null;
+'use strict';
+let is = x => x!=null;
+let split = x => 'string'==typeof x ? x.replace(/^\w/,'.$&').match(/\W?\w+/g) : x;
 
-  let split = x => 'string'==typeof x ? x.replace(/^\w/,'.$&').match(/\W?\w+/g) : x;
-
-  function set(o, path, val) {
-      let arr = split(path);
-      if(val===get(o, arr))
-        return false;
-
-      let last = arr.pop().slice(1);
-      arr.reduce((m, chunk) => {
-        let x = chunk.slice(1);
-        return x in m ? m[x] : (m[x] = chunk[0]=='[' ? [] : {});
-      }, o)[last] = val;
-      return true;
-    }
-
-  function remove(o, path) {
-      let arr = split(path);
-      let last = arr.pop().slice(1);
-      let prev = get(o, arr);
-      if(is(prev))
-        return prev.splice ? !!prev.splice(+last, 1) : (delete prev[last]);
+function set(o, path, val) {
+    let arr = split(path);
+    if(val===get(o, arr))
       return false;
-    }
 
-  function get(o, path, val) {
-      split(path).every(x => is(o=o[x.slice(1)]));
-      return is(o) ? o : val;
-    }
+    let last = arr.pop().slice(1);
+    arr.reduce((m, chunk) => {
+      let x = chunk.slice(1);
+      return x in m ? m[x] : (m[x] = chunk[0]=='[' ? [] : {});
+    }, o)[last] = val;
+    return true;
+  }
 
-  let dot = {get:get, set:set, remove:remove};
+function remove(o, path) {
+    let arr = split(path);
+    let last = arr.pop().slice(1);
+    let prev = get(o, arr);
+    if(is(prev))
+      return prev.splice ? !!prev.splice(+last, 1) : (delete prev[last]);
+    return false;
+  }
 
-  if('undefined'!=typeof module && module.exports)
-    module.exports = dot;
-  else if('function'==typeof define)
-    define([], () => dot);
-  else
-    root.dot = dot;
-})(this);
+function get(o, path, val) {
+    split(path).every(x => is(o=o[x.slice(1)]));
+    return is(o) ? o : val;
+  }
+
+exports.get = get;
+exports.set = set;
+exports.remove = remove;
